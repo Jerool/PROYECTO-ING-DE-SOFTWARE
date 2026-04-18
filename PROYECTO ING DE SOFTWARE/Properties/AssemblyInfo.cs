749f4d85e897a676aca6d4adb6b6235f972b7f64
Jeremias <jerool747@gmail.com>
2026-04-18 16:29:53 -0300

    Add project files.

---
+++ PROYECTO ING DE SOFTWARE/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PROYECTO ING DE SOFTWARE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://dsc.gg/kernelos")]
+[assembly: AssemblyProduct("PROYECTO ING DE SOFTWARE")]
+[assembly: AssemblyCopyright("Copyright © https://dsc.gg/kernelos 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4312ec5d-0c95-4837-96db-871e3ec4bd68")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
