00d2d861acb4803033d4f7466786841e770b551e
Jeremias <jerool747@gmail.com>
2026-04-20 09:01:24 -0300

    pusheo prueba

--- PROYECTO ING DE SOFTWARE/FRMIniciarSesion.Designer.cs
+++ PROYECTO ING DE SOFTWARE/FRMIniciarSesion.Designer.cs
@@ -33,6 +33,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtContraseña = new System.Windows.Forms.TextBox();
             this.btnAcceder = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtDNI
@@ -78,11 +79,21 @@
             this.btnAcceder.Text = "Acceder";
             this.btnAcceder.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FRMIniciarSesion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(475, 273);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAcceder);
             this.Controls.Add(this.txtContraseña);
             this.Controls.Add(this.label2);
@@ -102,6 +113,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtContraseña;
         private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.Button button1;
     }
 }
 
