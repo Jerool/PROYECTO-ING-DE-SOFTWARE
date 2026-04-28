89a501deb51534dff3b9fd9be6e3f9431bc0dedb
Lauty0630 <lautaronahuel.vergara@alumnos.uai.edu.ar>
2026-04-28 12:31:49 -0300

    Creacion de Servicio Auditoria y cambios en BLLBitacora

---
+++ Servicios/Auditoria_GV42.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class Auditoria_GV42
+    {
+        private static Auditoria_GV42 _Instancia;
+        private readonly GestorBitacora_GV42 _GestorBitacora;
+        private Auditoria_GV42()
+        {
+            _GestorBitacora = new GestorBitacora_GV42();
+        }
+
+        public static Auditoria_GV42 Instancia
+        {
+            get
+            {
+                if (_Instancia == null)
+                    _Instancia = new Auditoria_GV42();
+                return _Instancia;
+            }
+        }
+        public void RegistrarEvento(string login, string modulo, string evento, string criticidad)
+        {
+            var registro = new Bitacora
+            {
+                Login = login,
+                Modulo = modulo,
+                Evento = evento,
+                Criticidad = criticidad,
+                FechaHora = DateTime.Now
+            };
+            _GestorBitacora.Guardar(registro);
+        }
+
+        public List<Bitacora> Listar()
+        {
+            return _GestorBitacora.Listar();
+        }
+    }
+}
